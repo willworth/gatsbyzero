a54fd1d5bd21c0513c28cce9addf7cea07ce6905
Will Worth <will.worth@gmail.com>
2018-08-11 16:32:30 +0200

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteMetadata: {
+      title: 'Worth Coding',
+      author: 'Will Worth',
+      description: 'A coding blog.',
+      siteUrl: 'https://github.com/willworth/',
+    }
+}
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Header from '../components/header';
+// import About from 'about';
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <p>Testing</p>
+        
+
+
+
+        
+      </div>
+    )
+  }
+}
+
